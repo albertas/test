e9510761a9295121e7b9a6cc4f71f35158d71df1
Albertas Gimbutas <albertasgim@gmail.com>
2010-02-09 08:38:12 +0200

    Testing commit

---
+++ docstringpvz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Labas, pasauli.
+
+Demonstracinė programėlė Python kursui.
+
+Autorius -- Marius Gedminas
+"""
+
+def main():
+    """Spausdina ekrane žodžius "Hello, world!"."""
+    print "Hello, world!"
+
+if __name__ == "__main__":
+    main()
+
